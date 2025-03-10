4a176fd3e3d50edb787897112b73109276e95551
bimmbim <bimabaraja76@gmail.com>
2025-03-10 11:25:19 +0700

    function berparameter

---
+++ p2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+float panjang, lebar; //var globar
+
+void inputData(){
+    cout << "Masukkan panjangnya =";
+    cin >> panjang;
+
+
+    cout << "Masukkan lebarnya = ";
+    cin >> lebar;
+}
+
+float fungsiLuas(){
+    return panjang * lebar;
+}
+
+float hitungLuasBerparameter(float a, float b){
+    return a * b;
+}
+
+void tampilkanFungsiLuas(){
+    cout << "Luas Persegi panjang ( fungsi tidak berparameter): " << fungsiLuas()<< endl;
+}
+
+void tampilkanLuasBerparameter(){
+    cout << "Luas persegi panjang (fungsi berparameter): " << hitungLuasBerparameter(panjang, lebar);
+}
+
+int main()
+{
+    inputData();
+    tampilkanFungsiLuas();
+    tampilkanLuasBerparameter();
+}
